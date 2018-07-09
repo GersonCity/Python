b5a95c82d80f196724ec5bcb9b575980b708887f
Gerson <exgciud@dom_bci_prod.cl>
2018-07-09 12:43:49 -0400

    Intro agregado

---
+++ intro.py
@@ -0,0 +1,14 @@
+
+
+# import my_module as mm  # Le podemos poner alias a los modulos para llamarlos mas facil
+# # from my_module import find_index, test  ''' otra manera de llamar al modulo, pero especificando que se necesita llamar. '''
+# import sys
+
+# cursos = ['Historia', 'Arte', 'Fisica', 'Computacion']
+
+# index = mm.find_index(cursos, 'Arte')
+# print(index)
+# # print(test)
+
+# print(sys.path)
+# '''con eso vemos la ruta donde esta trayendo los modulos '''
