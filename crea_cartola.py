4c56714026768dc28856094515d2823cc99b05d0
GersonCity <gerson.ciudad@gmail.com>
2018-07-14 01:04:44 -0400

    Computer 2 update

--- crea_cartola.py
+++ crea_cartola.py
@@ -16,6 +16,3 @@ pdf.set_xy(0, 0)
 pdf.set_font('arial', 'B', 13.0)
 pdf.cell(ln=0, h=5.0, align='L', w=0, txt="Archivo Dummy para que proceso de control M no se cancele", border=0)
 pdf.output(Filename, 'F')
-
-
-aaaaaaaaaaaaaaaa
