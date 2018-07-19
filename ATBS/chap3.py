63d431f5c864eedc7b1abc4a8ced3f11804f94c7
GersonCity <gerson.ciudad@gmail.com>
2018-07-18 22:17:02 -0400

    commit home

--- ATBS/chap3.py
+++ ATBS/chap3.py
@@ -119,3 +119,51 @@ print(eggs_)
 
 
 # Exception Handling
+
+# NO hacer
+def spam(divideBy):
+    return 42 / divideBy
+
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+
+
+# SI hacer
+def spam(divideBy):
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+
+
+# Guess the Number
+# This is a guess the number game.
+import random
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20.')
+
+# Ask the player to guess 6 times.
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break    # This condition is the correct guess!
+
+if guess == secretNumber:
+    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
