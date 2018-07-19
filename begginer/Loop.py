a4c66cd7c0c4bd3d71a8efa6ca53221ecd5bedd6
GersonCity <gerson.ciudad@gmail.com>
2018-07-18 23:58:39 -0400

    home

--- begginer/Loop.py
+++ begginer/Loop.py
@@ -2,27 +2,27 @@
 nums = [1, 2, 3, 4, 5]
 
 # Si encuentra el numero sale del loop
-# for num in nums:
-#     if num == 3:
-#         print(str(num) + ' ' + 'Found!')
-#         break
-#     print(num)
+for num in nums:
+    if num == 3:
+        print(str(num) + ' ' + 'Found!')
+        break
+    print(num)
 
 # # Si encuentra numero, imprime y continua con los demas numeros
-# for num in nums:
-#     if num == 3:
-#         print(str(num) + ' ' + 'Found!')
-#         continue
-#     print(num)
+for num in nums:
+    if num == 3:
+        print(str(num) + ' ' + 'Found!')
+        continue
+    print(num)
 
 # loop dentro de otro loop, el siguiente loop nos todas las posibles combinaciones
-# for num in nums:
-#     for letter in 'abc':
-#         print(num, letter)
+for num in nums:
+    for letter in 'abc':
+        print(num, letter)
 
 # imprime del 1 al 10
-# for i in range(1, 11):
-#     print(i)
+for i in range(1, 11):
+    print(i)
 
 # while loop
  x = 0
@@ -31,7 +31,7 @@ nums = [1, 2, 3, 4, 5]
 #     print(x)
 #     x += 1
 
-# loop infinito   CNTRL + C para cancelar
+# loop infinito   CNTRL + pause(-) para cancelar
  while True:
      print(x)
      x += 1
