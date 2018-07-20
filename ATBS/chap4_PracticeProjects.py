276e4a8f604601c68c9b4e5a08f0f69e662ae526
GersonCity <gerson.ciudad@gmail.com>
2018-07-20 18:43:44 -0400

    asdf

--- ATBS/chap4_PracticeProjects.py
+++ ATBS/chap4_PracticeProjects.py
@@ -1,44 +1,17 @@
-# Comma Code
-
 # Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted before the last item
-
-spam = ['apples', 'bananas', 'tofu', 'cats', 'dogs']
-
-
-def commaCode(formatLista):
-
-    popped = formatLista.pop()  # toma el ultimo valor de la lista y lo guarda en una variable
-    # ultimoValor = 'and' + r.rsplit(',', 1)[1]  # obtengo el ultimo valor y le agrego un "and" antes.
-    ultimoValor = 'and ' + popped
-    formatLista.append(ultimoValor)
-    result = ', '.join(formatLista)  # Dejo la lista como un string y le agrego comas
-    print(result)
+import copy
+spam = ['apples', 'bananas', 'tofu', 'cats']
 
 
-# commaCode(spam)
+def commaCode(*formatLista):
+    lista = copy.copy(formatLista)
+    r = ', '.join(*formatLista)
+    ultimoValor = 'and' + r.rsplit(',', 1)[1]
+    # formatLista.insert(1, 'chicken')
+    print(formatLista)
+    # r_add = r.insert(-2, 'and')
+    print(r)
+    print(ultimoValor)
 
 
-# Character Picture Grid
-    #
-    #
-    #
-    #
-    # Y
-grid = [['.', '.', '.', '.', '.', '.'],  # <------- X
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
-
-corazon = []
-n = len(grid)
-print(n)
-
-for a in grid[x][y]:
-    print(a)
-    for b in grid[x][y]:
-        print(a, b)
+commaCode(spam)
